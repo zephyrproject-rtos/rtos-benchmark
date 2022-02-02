55d06bcc23bc451412a8c812a364b3a6b0987783
Ederson de Souza <ederson.desouza@intel.com>
2022-02-02 10:35:37 -0800

    tests: Update semaphore take/release test
    The "no context switch" benchmarking of semaphore take/release was
    updated to collect average, minimum and maximum times for a sequence of
    operations. Between each operation, a pointer chasing algorithm performs
    some unrelated work to avoid priming cache and pipeline.
    
    Signed-off-by: Ederson de Souza <ederson.desouza@intel.com>

--- src/bench_sem_signal_release_test.c
+++ src/bench_sem_signal_release_test.c
@@ -10,9 +10,8 @@
 #include "bench_api.h"
 #include "bench_utils.h"
 
-
-/* Number of give / take cycles on semaphore */
-#define NUM_TEST_SEM 1000
+uint32_t take_times[ITERATIONS];
+uint32_t give_times[ITERATIONS];
 
 /**
  * @brief Test main function
@@ -23,42 +22,52 @@ void bench_sem_signal_release()
 {
 	int i;
 	unsigned int diff;
+	uint64_t avg, min, max;
 	bench_time_t timestamp_start;
 	bench_time_t timestamp_end;
 
 	/* Measure average semaphore signal time */
 	bench_timing_start();
 
-	timestamp_start = bench_timing_counter_get();
-
-	for (i = 0; i < NUM_TEST_SEM; i++) {
+	for (i = 0; i < ITERATIONS; i++) {
+		timestamp_start = bench_timing_counter_get();
 		bench_sem_give(0);
+		timestamp_end = bench_timing_counter_get();
+		diff = bench_timing_cycles_get(&timestamp_start, &timestamp_end);
+		give_times[i] = diff;
+		thinker();
 	}
-
-	timestamp_end = bench_timing_counter_get();
 
 	bench_timing_stop();
 
-	diff = bench_timing_cycles_get(&timestamp_start, &timestamp_end);
-
-	PRINT_STATS("Average semaphore signal time", diff / NUM_TEST_SEM);
+	bench_stats(give_times, ITERATIONS, &avg, &min, &max);
+	BENCH_PRINTF("Semaphore give cycles (no context switch) [average, min, max],"
+		     " %llu, %llu, %llu, Time, %llu %llu %llu\n", avg, min, max,
+		     bench_timing_cycles_to_ns(avg),
+		     bench_timing_cycles_to_ns(min),
+		     bench_timing_cycles_to_ns(max));
 
 	/* Measure average semaphore test time */
 	bench_timing_start();
 
-	timestamp_start = bench_timing_counter_get();
 
-	for (i = 0; i < NUM_TEST_SEM; i++) {
+	for (i = 0; i < ITERATIONS; i++) {
+		timestamp_start = bench_timing_counter_get();
 		bench_sem_take(0);
+		timestamp_end = bench_timing_counter_get();
+		diff = bench_timing_cycles_get(&timestamp_start, &timestamp_end);
+		take_times[i] = diff;
+		thinker();
 	}
-
-	timestamp_end = bench_timing_counter_get();
 
 	bench_timing_stop();
 
-	diff = bench_timing_cycles_get(&timestamp_start, &timestamp_end);
-
-	PRINT_STATS("Average semaphore test time", diff / NUM_TEST_SEM);
+	bench_stats(take_times, ITERATIONS, &avg, &min, &max);
+	BENCH_PRINTF("Semaphore take cycles (no context switch) [average, min, max],"
+		     " %llu, %llu, %llu, Time, %llu %llu %llu\n", avg, min, max,
+		     bench_timing_cycles_to_ns(avg),
+		     bench_timing_cycles_to_ns(min),
+		     bench_timing_cycles_to_ns(max));
 }
 
 /**
@@ -68,7 +77,7 @@ void bench_sem_signal_release_init(void *param)
 {
 	bench_timing_init();
 
-	bench_sem_create(0, 0, NUM_TEST_SEM);
+	bench_sem_create(0, 0, ITERATIONS);
 
 	bench_sem_signal_release();
 }
